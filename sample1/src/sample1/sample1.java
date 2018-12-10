5a8ff7ba847d3f4a56dfd67bc26a2616cbea88e6
mihir <mihir@mihir-NE56R>
2018-12-10 11:03:28 +0530

    added hello client

--- sample1/src/sample1/sample1.java
+++ sample1/src/sample1/sample1.java
@@ -6,6 +6,7 @@ public class sample1 {
 		// TODO Auto-generated method stub
 		System.out.println("sample1");
 		System.out.println("added by client");
+		System.out.println("hello client");
 	}
 
 }
