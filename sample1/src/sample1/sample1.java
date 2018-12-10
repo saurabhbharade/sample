b057fe711c0b6e908e82e0251cc2781db3265619
saurabh <saurabhbharade@gmail.com>
2018-12-10 11:37:31 +0530

    restart task

--- sample1/src/sample1/sample1.java
+++ sample1/src/sample1/sample1.java
@@ -5,9 +5,6 @@ public class sample1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sample1");
-		System.out.println("added by client");
-		System.out.println("hello client");
-		System.out.println("hello client"); //server
 	}
 
 }
